4e08ae3dce17e6c1af0510a02989fc9082d6b444
Sayantan Samanta <90416470+Sayantan2k24@users.noreply.github.com>
2025-06-17 12:54:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def home():
     return "<b> Welcome to my project!! </b> <br/>"
    
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
